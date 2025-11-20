92fc1c4f6fa76e933095deae9d794000d5c035f1
safes0und <kirill.pozniak72@yandex.ru>
2025-11-20 17:00:51 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.get("/")
 def hello():
-    return {"status": "ok", "msg": "Diploma test"}
+    return {"status": "ok", "msg": "Diploma test!"}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
